730ef95eea7a42fc2dbb1cdae06fdc328d5db976
deepthi1 <deepthi1@gmail.com>
2017-11-07 10:05:26 +0000

    display no posts available

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 def post_list(request):
-	print('------------------ Hey post_list is called ------------')
-	print(request.GET)
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 	
 	
 def post_detail(request, **kwargs):
-	print('Keyword Arguments: ', kwargs)
 	post_id = kwargs.get('pk')
-	print('------------------ Hey post_detail is called ------------')
 	post = get_object_or_404(Post, id=post_id)
 	return render(request, 'blog/post_detail.html', {'post': post})
